323a696733ea42b5b68e4653fd4d3026bc24a6f3
Mario Juric <mjuric@lsst.org>
2013-12-17 16:14:10 -0600

    basic lsst-build build is now functional

--- python/lsst/ci/build.py
+++ python/lsst/ci/build.py
@@ -1,8 +1,13 @@
 #############################################################################
 # Builder
+import eups
+
 import collections
+import subprocess
 import textwrap
-import os
+import os, stat, sys
+import pipes
+import time
 
 Product = collections.namedtuple('Product', ['name', 'sha1', 'version'])
 
@@ -24,48 +29,105 @@ class Builder(object):
 
 	def build_all(self):
 		for name in self.products:
-			self._build(name)
+			if not self._build(name):
+				return False
 
 	def _build(self, name):
 		product = self.products[name]
 
 		# run the pkgbuild sequence for the product
-		buildfile = os.path.join(self.build_dir, product.name, '_build.sh')
+		productdir = os.path.abspath(os.path.join(self.build_dir, product.name))
+		buildscript = os.path.join(productdir, '_build.sh')
+		logfile = os.path.join(productdir, '_build.log')
 
 		# compute the setup invocations for dependencies
 		setups = []
+#		table_fn = os.path.join(productdir, 'ups', '%s.table' % name)
+#		if os.path.isfile(table_fn):
+#			for dep in eups.table.Table(table_fn).dependencies(eups.Eups()):
+#				depname = dep[0].name
+#				if depname in self.products:
+#					setups.append("setup --type=build --j %(name)-15s %(version)s" % self.products[depname]._asdict())
 		for dep in self.products.itervalues():
 			if dep.name == product.name:
 				break
 			setups.append("setup --type=build --j %(name)-15s %(version)s" % dep._asdict())
 
-		with open(buildfile, 'w') as fp:
+		# create the buildscript
+		with open(buildscript, 'w') as fp:
 			text = textwrap.dedent(
 			"""\
 			#!/bin/bash
-			
+
+			# redirect stderr to stdin
+			exec 2>&1
+
+			# stop on any error
+			set -ex
+
+			cd %(productdir)s
+
+			# clean up the working directory
+			git reset --hard
+			git clean -d -f -q -e '_build.*'
+
+			# prepare
 			env PRODUCT=%(product)s VERSION=%(version)s   pkgbuild prep
 			
+			# setup dependencies (if any)
 			%(setups)s
 			
 			setup -j -r .
-			
+
+			# build
 			env PRODUCT=%(product)s VERSION=%(version)s   pkgbuild config
 			env PRODUCT=%(product)s VERSION=%(version)s   pkgbuild build
 			env PRODUCT=%(product)s VERSION=%(version)s   pkgbuild install
+			env PRODUCT=%(product)s VERSION=%(version)s   pkgbuild eups_declare
 			""" %	{
 					'product': product.name,
 					'version': product.version,
+					'productdir' : productdir,
 					'setups': '\n			'.join(setups)
 				}
 			)
 
 			fp.write(text)
 
-		# execute the build file
-		TODO
+		# Make executable (equivalent of 'chmod +x $buildscript')
+		st = os.stat(buildscript)
+		os.chmod(buildscript, st.st_mode | stat.S_IXUSR | stat.S_IXGRP | stat.S_IXOTH)
 
-		print buildfile, "constructed."
+		# Run the build script
+		sys.stderr.write('%15s: ' % name)
+		with open(logfile, 'w') as logfp:
+			# execute the build file from the product directory, capturing the output and return code
+			t0 = t = time.time()
+			process = subprocess.Popen(buildscript, stdout=subprocess.PIPE, stderr=subprocess.PIPE, cwd=productdir)
+			for line in iter(process.stdout.readline,''):
+				logfp.write(line)
+				
+				# throttle progress reporting
+				t1 = time.time()
+				if (t1 - t) >= 1:
+					sys.stderr.write('+')
+					t = t1
+
+		retcode = process.poll()
+		if retcode:
+			print >>sys.stderr, " ERROR (%d sec)." % (time.time() - t0)
+			print >>sys.stderr, "*** error building product %s." % product.name
+			print >>sys.stderr, "*** exit code = %d" % retcode
+			print >>sys.stderr, "*** log is in %s" % logfile
+			print >>sys.stderr, "*** last few lines:"
+
+			os.system("tail -n 10 %s | sed -e 's/^/:::::  /'" % pipes.quote(logfile))
+
+			return False
+		else:
+			print >>sys.stderr, " ok (%.1f sec)." % (time.time() - t0)
+
+		return True
 
 	@staticmethod
 	def run(args):
