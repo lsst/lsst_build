0eca2a556a096251d9e072bb21800ebc2840ae2b
Tim Jenness <tjenness@lsst.org>
2026-07-21 11:29:25 -0700

    Run ruff format (preferred over black)

--- python/lsst/ci/models.py
+++ python/lsst/ci/models.py
@@ -230,9 +230,7 @@ class GitHubPR(BaseModel):
     def __str__(self) -> str:
         """One liner for logging / printing."""
         return (
-            f"PR #{self.pr_number} "
-            f"in {self.owner}/{self.repo} "
-            f"for product {self.product_name}: {self.title}"
+            f"PR #{self.pr_number} in {self.owner}/{self.repo} for product {self.product_name}: {self.title}"
         )
 
     def post_github_status(self, *, state: str, description: str, agent: str) -> None:
